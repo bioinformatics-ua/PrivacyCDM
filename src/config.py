5ca7651564e5f30870bc188d4b5d4476693459db
João Almeida <joao.rafael.almeida@ua.pt>
2022-06-30 13:53:29 +0100

    Adding initial files

---
+++ src/config.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+basePath = Path(__file__).parent
+cdmSchemaPath = (basePath / "../aux/OMOP_CDMv5.4_Field_Level.csv").resolve()
---
+++ src/utils.py
@@ -0,0 +1,11 @@
+from config import cdmSchemaPath
+
+def readFields():
+	with open(cdmSchemaPath, 'r') as f:
+		lines = f.readlines()
+	for line in lines:
+		data = line.split(",")
+		if len(data) > 2:
+			print(data[0],data[1])
+
+readFields()
