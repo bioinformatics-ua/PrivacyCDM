8255330ff7a808220e4b32921bdd709c547295f7
jalmeida <joao.rafael.almeida@ua.pt>
2022-07-01 19:03:14 +0100

    Creating method to show mapped attributes in a user-friendly format to be added in the LaTeX document

--- src/main.py
+++ src/main.py
@@ -1,35 +1,73 @@
 import argparse
 import configparser
-import utils
+from pprint import pprint
+from utils import *
+from attributes import *
 
 def help(show=False):
-    parser = argparse.ArgumentParser(description="")
-    configs = parser.add_argument_group('Privacy-preserving meets OMOP CDM', 'This tool aims to measure the privacy levels in released data from OMOP CDM structure')
-    #configs.add_argument('-s', '--settings', dest='settings', \
-    #                    type=str, default="settings.ini", \
-    #                    help='The system settings file (default: settings.ini)')    
-    configs.add_argument('-rf', '--read-fields', default=False, action='store_true', \
-                         help='When active, it reads the OMOP CDM CSV file with all fields and print them (default: False)')
+	parser = argparse.ArgumentParser(description="")
+	configs = parser.add_argument_group('Core features', 'This tool aims to measure the privacy levels in released data from the OMOP CDM structure')
+	#configs.add_argument('-s', '--settings', dest='settings', \
+	#					type=str, default="settings.ini", \
+	#					help='The system settings file (default: settings.ini)')	
+	aux_methods = parser.add_argument_group('Auxiliary methods', 'Methods useful during the development of this tool that may not be necessary for the last version.')
+	aux_methods.add_argument('-rf', '--read-fields', default=False, action='store_true', \
+						 help='When active, it reads the OMOP CDM CSV file with all fields and print them (default: False)')
+	aux_methods.add_argument('-sa', '--show-attributes', default=False, action='store_true', \
+						 help='When active, it reads and prints the attributes defined hardcoded in file attibutes.py (default: False)')
 
+	aux_methods.add_argument('-la', '--latex-attributes', default=False, action='store_true', \
+						 help='When active, it reads and prints the attributes in a format easy to insert in the latex document (default: False)')
+	aux_methods.add_argument('-la-k', '--latex-attributes-key', default=False, action='store_true', \
+						 help='When active, it reads and prints the key attributes in a format easy to insert in the latex document (default: False)')
+	aux_methods.add_argument('-la-q', '--latex-attributes-quasi-identifier', default=False, action='store_true', \
+						 help='When active, it reads and prints the quasi-identifier attributes in a format easy to insert in the latex document (default: False)')
+	aux_methods.add_argument('-la-s', '--latex-attributes-sensitive', default=False, action='store_true', \
+						 help='When active, it reads and prints the sensitive attributes in a format easy to insert in the latex document (default: False)')
 
-    if show:
-        parser.print_help()
-    return parser.parse_args()
+	if show:
+		parser.print_help()
+	return parser.parse_args()
 
 def readSettings(settingsFile):
-    configuration = configparser.ConfigParser()
-    configuration.read(settingsFile)
-    if not configuration:
-        raise Exception("The settings file was not found!")
-    return configuration
+	configuration = configparser.ConfigParser()
+	configuration.read(settingsFile)
+	if not configuration:
+		raise Exception("The settings file was not found!")
+	return configuration
 
+def coreMethods(args):
+	pass
+
+def auxMethods(args):	
+	if args.read_fields:
+		readFields()
+
+	if args.show_attributes:
+		pprint(processAttributes(keyAttributes))
+		pprint(processAttributes(sensitiveAttributes))
+		pprint(processAttributes(quasiIdentifiers))
+
+	if args.latex_attributes:
+		print(attibutesToLatexTable(processAttributes(keyAttributes)))
+		print(attibutesToLatexTable(processAttributes(sensitiveAttributes)))
+		print(attibutesToLatexTable(processAttributes(quasiIdentifiers)))
+
+	if args.latex_attributes_key:
+		print(attibutesToLatexTable(processAttributes(keyAttributes)))
+
+	if args.latex_attributes_quasi_identifier:
+		print(attibutesToLatexTable(processAttributes(sensitiveAttributes)))
+		
+	if args.latex_attributes_sensitive:
+		print(attibutesToLatexTable(processAttributes(quasiIdentifiers)))
+		
 
 def main():
-    args = help()
-    settings = readSettings(args.settings)
-
-    if args.read_fields:
-        utils.readFields()
+	args = help()
+	#settings = readSettings(args.settings)
+	coreMethods(args)
+	auxMethods(args)
 
 if __name__ == "__main__":
-    main()
+	main()
--- src/utils.py
+++ src/utils.py
@@ -7,3 +7,25 @@ def readFields():
 		data = line.split(",")
 		if len(data) > 2:
 			print(data[0],data[1])
+
+def processAttributes(listOfAttributes):
+	dictOfAttributes = {}
+	for table, field in listOfAttributes:
+		table = table.lower()
+		if table not in dictOfAttributes:
+			dictOfAttributes[table] = []
+		dictOfAttributes[table].append(field.lower())
+	return dictOfAttributes
+
+def attibutesToLatexTable(dictOfAttributes):
+	for table in dictOfAttributes:
+		tableToPrint = table.replace("_", "\\_")
+		print("\t" + tableToPrint, end = ' & ')
+		tableFields = ""
+		for field in dictOfAttributes[table]:
+			tableFields += field
+			tableFields += ", "
+		tableFields = tableFields[:-2]
+		tableFields += " \\\\  \\hline"
+		tableFields = tableFields.replace("_", "\\_")
+		print(tableFields)
