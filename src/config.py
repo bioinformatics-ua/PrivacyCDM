c9d4bfccf782434066972c29485bebc1e9c893b3
jalmeida <joao.rafael.almeida@ua.pt>
2022-07-03 20:28:29 +0100

    Add path for plugins

--- src/config.py
+++ src/config.py
@@ -2,3 +2,4 @@ from pathlib import Path
 
 basePath = Path(__file__).parent
 cdmSchemaPath = (basePath / "../aux/OMOP_CDMv5.4_Field_Level.csv").resolve()
+pluginsPath = (basePath / "plugins").resolve()
